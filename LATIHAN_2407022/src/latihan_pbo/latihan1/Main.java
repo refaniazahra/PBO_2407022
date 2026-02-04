62a50ca9ad5bf945c62651095844805eb1a994c5
refaniazahra <revani.jatibarang12@gmail.com>
2026-02-04 09:40:06 +0700

    latihan cafe

--- LATIHAN_2407022/src/latihan_pbo/latihan1/Main.java
+++ LATIHAN_2407022/src/latihan_pbo/latihan1/Main.java
@@ -1,7 +1,49 @@
 package latihan_pbo.latihan1;
 
+class Cafe{
+    String nama;
+    String alamat;
+
+    Cafe(String nama, String alamat){
+    this.nama = nama;
+    this.alamat = alamat;
+    System.out.println(nama);
+    System.out.println(alamat);
+    }
+
+    // method
+    void buka(){
+    System.out.println("Cafe" + nama + "Sudah buka");
+}
+}
+
+// constructor
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Praktikum Pertama");
+
+        // Cafe cafe1 = new Cafe();
+        Cafe cafe4 = new Cafe( "Kopkar",  "Indramayu");
+        cafe4.buka();
+        // cafe1.nama = "Kopi Kenangan";
+        // cafe1.alamat = "Indramayu";
+
+        // Cafe cafe2 = new Cafe();
+        // cafe2.nama = "KOPTE";
+        // cafe2.alamat = "Bandung";
+
+        // Cafe cafe3 = new Cafe();
+        // cafe3.nama = "Kopi Dari Hati";
+        // cafe3.alamat = "Jatibarang";
+
+        // System.out.println("Nama Cafenya adalah : " + cafe1.nama);
+        // System.out.println("Alamatnya ada di : " + cafe1.alamat);
+
+        // System.out.println("Nama Cafenya adalah : " + cafe2.nama);
+        // System.out.println("Alamatnya ada di : " + cafe2.alamat);
+
+        // System.out.println("Nama Cafenya adalah : " + cafe3.nama);
+        // System.out.println("Alamatnya ada di : " + cafe3.alamat);
+        
     }
 }
